6342d05430b4a2ce185e0dbf8190f23e72012f7e
glory kuria <kuriaglory@gmail.com>
2024-02-08 08:15:26 +0300

    kotlin assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var name = "AmaliaFaith"
+    println(name)
+    var age: Byte = 16
+    println(age)
+    var phoneNumber = "254712349870"
+    println(phoneNumber)
+    var weightInKg: Float = 50.2F
+    println(weightInKg)
+    var Kenyan: Boolean = true
+        println(true)
+    var notKenyan: Boolean= false
+    println(notKenyan)
+
+}
